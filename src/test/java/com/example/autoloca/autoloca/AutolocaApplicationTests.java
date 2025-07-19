9927469b1dafa9ece4e48d13704e909031b2b43a
carlosEduardoRincon <carloscaca147@gmail.com>
2025-07-19 14:55:54 -0300

    feat: estrutura do projeto

---
+++ src/test/java/com/example/autoloca/autoloca/AutolocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoloca.autoloca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutolocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
